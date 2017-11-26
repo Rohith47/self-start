3ced99c7bba3967e0af4907662ddd8b1aabbe95d
Rohith S R <rohith47@gmail.com>
2017-11-26 21:38:31 +0530

    M/c learnig poc - Linear regression

--- server.js
+++ server.js
@@ -29,3 +29,68 @@ app.listen(port);
 
 console.log('todo list RESTful API server started on: ' + port);
 
+const ml = require('ml-regression');
+const csv = require('csvtojson');
+const SLR = ml.SLR; // Simple Linear Regression
+
+const csvFilePath = 'Advertising.csv'; // Data
+var csvData = [], // parsed Data
+    X = [], // Input
+    y = []; // Output
+
+var regressionModel;
+
+const readline = require('readline'); // For user prompt to allow predictions
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+csv()
+    .fromFile(csvFilePath)
+    .on('json', function(jsonObj) {
+    csvData.push(jsonObj);
+}).on('done',function() {
+    dressData(); // To get data points from JSON Objects
+performRegression();
+});
+
+function performRegression() {
+    regressionModel = new SLR(X, y); // Train the model on training data
+    console.log(regressionModel.toString(3));
+    predictOutput();
+}
+
+function dressData() {
+    /**
+     * One row of the data object looks like:
+     * {
+     *   TV: "10",
+     *   Radio: "100",
+     *   Newspaper: "20",
+     *   "Sales": "1000"
+     * }
+     *
+     * Hence, while adding the data points,
+     * we need to parse the String value as a Float.
+     */
+    csvData.forEach(function(row) {
+        X.push(f(row.radio));
+    y.push(f(row.sales));
+});
+}
+
+function f(s) {
+    console.log("s is "+s);
+    console.log("floating s is "+parseFloat(s));
+    return parseFloat(s);
+}
+
+function predictOutput() {
+    rl.question('Enter input X for prediction (Press CTRL+C to exit) : ', function(answer){
+        console.log(`At X = ${answer}, y =  ${regressionModel.predict(parseFloat(answer))}`);
+    predictOutput();
+});
+}
+
